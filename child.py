d0bc46b6a2ea8eef17e0d302f3bda40238e840fe
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2017-08-23 20:02:48 +0900

    while loop version

--- child.py
+++ child.py
@@ -2,15 +2,16 @@ import socket
 from datetime import datetime
 from time import sleep
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-port = 10002
-s.bind(('', port))
-data, (host, port) = s.recvfrom(4096)
-print data
+    port = 10002
+    s.bind(('', port))
+    data, (host, port) = s.recvfrom(4096)
+    print data
 
-s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-port = 30000
-msg = "raspi[1] says " + "hello!"*30
-s2.sendto(msg, (host, port))
+    port = 30000
+    msg = "raspi[1] says " + "hello!"*30
+    s2.sendto(msg, (host, port))
