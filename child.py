a428fe81946986db24045732b39ebef25e2d323b
msrks <mshrrks@gmail.com>
2017-08-29 16:46:48 +0900

    fix typo #6

--- child.py
+++ child.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from time import sleep
 import subprocess
 
-src-port = 10002
+src_port = 10002
 
 cmd = 'cat /etc/dhcpcd.conf | grep ip_address=192 | cut -d "." -f4 | cut -d "/" -f1'
 _id = subprocess.check_output(cmd, shell=True)
@@ -11,7 +11,7 @@ id = _id[1:-1]
 msg = "raspi[" + id + "] says " + "hello!"*30
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', src-port))
+s.bind(('', src_port))
 
 while True:
     data, (host, port) = s.recvfrom(4096)
