6d398aa92a12b0a00dee3a1eeb7b9f6964e53f33
msrks <mshrrks@gmail.com>
2017-08-29 16:41:57 +0900

    #6

--- child.py
+++ child.py
@@ -1,14 +1,19 @@
 import socket
 from datetime import datetime
 from time import sleep
+import subprocess
 
-port = 10002
-msg = "raspi[1] says " + "hello!"*30
+src-port = 10002
+
+cmd = 'cat /etc/dhcpcd.conf | grep ip_address=192 | cut -d "." -f4 | cut -d "/" -f1'
+_id = subprocess.check_output(cmd, shell=True)
+id = _id[1:-1]
+msg = "raspi[" + id + "] says " + "hello!"*30
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', port))
+s.bind(('', src-port))
 
 while True:
     data, (host, port) = s.recvfrom(4096)
-    print data
+    #print data
     s.sendto(msg, (host, port))
