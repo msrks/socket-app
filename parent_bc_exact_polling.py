b213a5ed384a7e44a9fab96141cc23d68ac507e3
Masahiro Rikiso <mshrrks@gmail.com>
2017-09-05 13:47:41 +0900

    change timelimit as 0.9s

--- parent_bc_exact_polling.py
+++ parent_bc_exact_polling.py
@@ -18,6 +18,7 @@ msg = msg[:len_payload]
 dst_ip = sys.argv[1] # "192.168.88.255"
 num_children = int(sys.argv[2]) # 16
 interval = float(sys.argv[3]) # 0.1
+timelimit = 0.9
 
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -28,7 +29,7 @@ while True:
     s.sendto(msg, (dst_ip, dst_port))
     start = time()
 
-    @timeout_decorator.timeout(1, timeout_exception=StopIteration)
+    @timeout_decorator.timeout(timelimit, timeout_exception=StopIteration)
     def rcv_packets():
         for i in range(num_children):
             r = s.recv(4096)
