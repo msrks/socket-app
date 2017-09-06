46cf5925255b89b5e46f9f58ce41c64d70280276
msrks <mshrrks@gmail.com>
2017-09-06 12:25:48 +0900

    fix packet-size

--- child.py
+++ child.py
@@ -8,7 +8,7 @@ _id = subprocess.check_output(cmd, shell=True)
 id = _id[1:-1]
 msg = "raspi[" + id + "] says " + "hello!"*30
 #len_payload = 100-14-20-8-4 # eth14 ip20 udp8 fcs4
-len_payload = 84-14-20-8-4 # eth14 ip20 udp8 fcs4
+len_payload = 86-14-20-8-4 # eth14 ip20 udp8 fcs4
 msg = msg[:len_payload]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
