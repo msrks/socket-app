3ba7866249ea1113cabe6d12c6a361e056b4c60f
msrks <mshrrks@gmail.com>
2017-09-06 10:51:54 +0900

    cp parent_uc.py parent_uc_exact_polling.py

---
+++ parent_uc_exact_polling.py
@@ -0,0 +1,42 @@
+"""
+usage:
+$ python parent_uc.py <dst-ip> <src-port> <interval>
+$ python parent_uc.py 192.168.88.102 30003 0.1
+"""
+import socket
+from datetime import datetime
+from time import sleep, time
+import sys
+import timeout_decorator
+
+dst_port = 10002
+msg = "toooooooooooooooooooooth!"*12
+len_payload = 68-14-20-8-4 # eth14 ip20 udp8 fcs4
+msg = msg[:len_payload]
+
+dst_ip = sys.argv[1] # "192.168.88.1"
+src_port = int(sys.argv[2]) # 30003
+interval = float(sys.argv[3]) # 0.1
+
+while True:
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    now = datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")[:-3]
+
+    s.bind(('', src_port))
+    s.sendto(msg, (dst_ip, dst_port))
+    start = time()
+
+    @timeout_decorator.timeout(5, timeout_exception=StopIteration)
+    def rcv_packets():
+        r = s.recv(4096)
+        stop = time()
+        print now, r[:9], round(stop-start, 3), "s"
+
+    try:
+        rcv_packets()
+    except StopIteration:
+        print "timeout!!!!!"
+    finally:
+        print ""
+        sleep(interval)
