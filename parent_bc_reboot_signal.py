d614f9af4cecc95f202df254649d8c3da1afd0fc
msrks <mshrrks@gmail.com>
2017-09-28 14:46:32 +0900

    send reboot signal

--- parent_bc_reboot_signal.py
+++ parent_bc_reboot_signal.py
@@ -1,50 +1,21 @@
 #! -*- coding: utf-8 -*-
 """
 usage:
-$ python parent_bc_exact_polling.py <dst-ip> <num-child> <interval>
-$ python parent_bc_exact_polling.py 192.168.88.255 16 0.1
+$ python parent_bc_reboot_signal.py <dst-ip> <interval>
+$ python parent_bc_reboot_signal.py 192.168.0.255 0.1
 """
 import socket
-from datetime import datetime
-from time import sleep, time
+from time import sleep
 import sys
-import timeout_decorator
 
-src_port = 30000
+dst_ip = sys.argv[1] # "192.168.0.255"
 dst_port = 10002
-msg = "toooooooooooooooooooooth!"*12
-len_payload = 68-14-20-8-4 # eth14 ip20 udp8 fcs4
-msg = msg[:len_payload]
-
-dst_ip = sys.argv[1] # "192.168.88.255"
-num_children = int(sys.argv[2]) # 16
-interval = float(sys.argv[3]) # 0.1
-timelimit = 0.9
+interval = float(sys.argv[2]) # 0.1
+msg = 0x001a01e10a0b0c0d0e0f00000999e30046550100000000000001    
+msg = b'\x00\x1a\x01\xe1\x0a\x0b\x0c\x0d\x0e\x0f\x00\x00\x09\x99\xe3\x00\x46\x55\x01\x00\x00\x00\x00\x00\x00\x01'
 
 while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    now = datetime.now().strftime("%Y/%m/%d %H:%M:%S.%f")[:-3]
-
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    s.bind(('', src_port))
     s.sendto(msg, (dst_ip, dst_port))
-    start = time()
-
-    @timeout_decorator.timeout(timelimit, timeout_exception=StopIteration)
-    def rcv_packets():
-        log_message = ""
-        for i in range(num_children):
-            r = s.recv(4096)
-            stop = time()
-            _log = now + " " + r[:9] + " " + str(round(stop-start, 3)) + " s\n"
-            log_message += _log
-        print log_message
-
-    try:
-        rcv_packets()
-    except StopIteration:
-        print "timeout!!!!!\n"
-    finally:
-        response_time = time() - start
-        sleeptime = interval - response_time
-        sleep(sleeptime)
+    sleep(interval)
