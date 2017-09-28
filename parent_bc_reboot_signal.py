df93e6144fd06b5e5c97df4f4e7dd4a2b98b695b
msrks <mshrrks@gmail.com>
2017-09-28 14:47:43 +0900

    fix typo

--- parent_bc_reboot_signal.py
+++ parent_bc_reboot_signal.py
@@ -11,7 +11,7 @@ import sys
 dst_ip = sys.argv[1] # "192.168.0.255"
 dst_port = 10002
 interval = float(sys.argv[2]) # 0.1
-msg = 0x001a01e10a0b0c0d0e0f00000999e30046550100000000000001    
+
 msg = b'\x00\x1a\x01\xe1\x0a\x0b\x0c\x0d\x0e\x0f\x00\x00\x09\x99\xe3\x00\x46\x55\x01\x00\x00\x00\x00\x00\x00\x01'
 
 while True:
